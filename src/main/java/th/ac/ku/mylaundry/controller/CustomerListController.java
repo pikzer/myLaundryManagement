07b0593cb3cb4659a3995f04c4403785cfcbb908
pikzer <rujipas.m@ku.th>
2022-10-23 21:22:35 +0700

    update CusList

--- src/main/java/th/ac/ku/mylaundry/controller/CustomerListController.java
+++ src/main/java/th/ac/ku/mylaundry/controller/CustomerListController.java
@@ -2,6 +2,7 @@ package th.ac.ku.mylaundry.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,6 +13,7 @@ import th.ac.ku.mylaundry.model.Customer;
 import th.ac.ku.mylaundry.model.MemberPackage;
 import th.ac.ku.mylaundry.service.CustomerApiDataSource;
 import th.ac.ku.mylaundry.service.MemberPackageDataSource;
+import th.ac.ku.mylaundry.service.Validator;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,13 +23,21 @@ public class CustomerListController extends Navigator {
     @FXML
     TableView cusTable;
     @FXML
-    ComboBox serviceTypeCombo, serviceAddCombo, piecesCombo ;
+    Button addMemBtn, recBtn, invBtn;
+    @FXML
+    ComboBox<String> serviceTypeCombo;
+    @FXML
+    ComboBox<String> serviceAddCombo;
+    @FXML
+    ComboBox<Integer> piecesCombo ;
     @FXML
     TextField searchField, nameField, telField, emailField ;
     @FXML
     CheckBox memCheck ;
     @FXML
     TextArea adsTextArea ;
+    @FXML
+    Label totalLabel ;
 
 
 
@@ -44,7 +54,16 @@ public class CustomerListController extends Navigator {
     public void initialize() throws IOException {
         customerArrayList = CustomerApiDataSource.getCustomers();
         memberPackageArrayList = MemberPackageDataSource.getMemberPackage();
+        totalLabel.setText("0.00");
+        piecesCombo.setDisable(true);
+        serviceAddCombo.setDisable(true);
+        addMemBtn.setDisable(true);
+        recBtn.setDisable(true);
+        invBtn.setDisable(true);
+//        serviceAddCombo.getItems().clear();
+//        piecesCombo.getItems().clear();
         serviceTypeCombo.getItems().addAll(serviceType);
+        serviceTypeCombo.getSelectionModel().select(0);
 //        piecesCombo.getItems().addAll(piecesCombo);
         serviceAddCombo.getItems().addAll(serviceTypeFill());
         showTableCustomer(FXCollections.observableList(customerArrayList));
@@ -55,26 +74,45 @@ public class CustomerListController extends Navigator {
         });
         // TODO Listener Service ADD
 //        serviceTypeCombo.getSelectionModel().selectedItemProperty().
-
+        serviceAddCombo.setOnAction((event -> {
+//            System.out.println(serviceAddCombo.getSelectionModel().getSelectedItem().toString());
+            piecesCombo.getItems().addAll(piecesFill(serviceAddCombo.getSelectionModel().getSelectedItem()));
+        }));
+        piecesCombo.setOnAction((event -> {
+//            System.out.println(serviceAddCombo.getSelectionModel().getSelectedItem().toString());
+            for (MemberPackage p:memberPackageArrayList) {
+//                if(p.getService().equals(serviceAddCombo.getSelectionModel().getSelectedItem().toString()) &&
+//                p.getQuantity().equals(piecesCombo.getSelectionModel().getSelectedItem().toString())){
+//                    totalLabel.setText(p.getPrice().toString());
+//                }
+                if(p.getService().equals(serviceAddCombo.getSelectionModel().getSelectedItem())){
+                    if(p.getQuantity() == piecesCombo.getSelectionModel().getSelectedItem()){
+                        totalLabel.setText(p.getPrice().toString());
+                    }
+                }
+            }
+        }));
     }
 
     public void showTableCustomer(ObservableList<Customer> customerObservableList){
         cusTable.getColumns().clear();
-        TableColumn idCol = new TableColumn("ID");
-        TableColumn phoneCol = new TableColumn("Tel");
-        TableColumn emailCol = new TableColumn("Email");
-        TableColumn isMemberCol = new TableColumn("Status");
-        TableColumn memServiceCol = new TableColumn("Service Member");
-        TableColumn memCreditCol = new TableColumn("Balance");
+        TableColumn idCol = new TableColumn("ไอดี");
+        TableColumn<Customer, String> nameCol = new TableColumn<Customer, String>("ชื่อ");
+        TableColumn<Customer, String> phoneCol = new TableColumn<Customer, String>("เบอร์โทร");
+        TableColumn<Customer, String> emailCol = new TableColumn<Customer, String>("อีเมลล์");
+        TableColumn<Customer, Integer> isMemberCol = new TableColumn<Customer, Integer>("สถานะสมาชิก");
+        TableColumn<Customer, String> memServiceCol = new TableColumn<Customer, String>("ประเภท");
+        TableColumn<Customer, Integer> memCreditCol = new TableColumn<Customer, Integer>("คงเหลือ");
 
         idCol.setCellValueFactory(new PropertyValueFactory<Customer,Integer>("id"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<Customer,String>("name"));
         phoneCol.setCellValueFactory(new PropertyValueFactory<Customer,String>("phone"));
         emailCol.setCellValueFactory(new PropertyValueFactory<Customer,String>("email"));
         isMemberCol.setCellValueFactory(new PropertyValueFactory<Customer,Integer>("isMembership"));
         memServiceCol.setCellValueFactory(new PropertyValueFactory<Customer,String>("memService"));
         memCreditCol.setCellValueFactory(new PropertyValueFactory<Customer,Integer>("memCredit"));
 
-        cusTable.getColumns().addAll(idCol,phoneCol,emailCol,isMemberCol,memServiceCol,memCreditCol) ;
+        cusTable.getColumns().addAll(idCol,nameCol,phoneCol,emailCol,isMemberCol,memServiceCol,memCreditCol) ;
         cusTable.setItems(customerObservableList);
     }
 
@@ -82,13 +120,34 @@ public class CustomerListController extends Navigator {
         nameField.setText(newValue.getName());
         telField.setText(newValue.getPhone());
         emailField.setText(newValue.getEmail());
+        piecesCombo.setDisable(false);
+        serviceAddCombo.setDisable(false);
+        addMemBtn.setDisable(false);
+        recBtn.setDisable(false);
+        invBtn.setDisable(false);
     }
 
+    public void onClickAddEditCus(ActionEvent event) throws IOException {
+        if(cusTable.getSelectionModel().isEmpty()){
+            // ADD
+            if(!nameField.getText().isEmpty() && Validator.isPhoneNumber(telField.getText())){
+                CustomerApiDataSource.addNewCustomer(nameField.getText(),telField.getText());
+                clearAll();
+            }
+            // UPDATE
+            else{
+
+            }
+        }
+    }
     public void onClickAnchor() throws IOException {
         clearAll();
     }
     public void clearAll() throws IOException {
         cusTable.getSelectionModel().clearSelection();
+        piecesCombo.getItems().clear();
+//        serviceAddCombo.getItems().clear();
+        serviceTypeCombo.getItems().clear();
         nameField.clear();
         telField.clear();
         emailField.clear();
--- src/main/java/th/ac/ku/mylaundry/service/CustomerApiDataSource.java
+++ src/main/java/th/ac/ku/mylaundry/service/CustomerApiDataSource.java
@@ -1,12 +1,15 @@
 package th.ac.ku.mylaundry.service;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import th.ac.ku.mylaundry.model.Customer;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 import static th.ac.ku.mylaundry.service.ApiUtil.decodeRespond;
@@ -42,6 +45,37 @@ public class CustomerApiDataSource extends ApiCall {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void addNewCustomer(String name, String phone){
+        var urlParameters = "name="+name+"&"+"phone="+phone;
+        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+        try {
+            URL url = new URL(baseURL+"customers");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("User-Agent", "Java client");
+            conn.setRequestProperty("Authorization","Bearer "+ token);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            try (var wr = new DataOutputStream(conn.getOutputStream())) {
+                wr.write(postData);
+            }
+            String j = decodeRespond(new InputStreamReader(conn.getInputStream()));
+            JSONObject jsonObject = new JSONObject(j);
+            System.out.println(jsonObject);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static boolean updateCustomer(){
+        return false;
+    }
+
+    public static boolean addMembership(String phone){
+        return false ;
     }
 
 }
